34952f42d44f7f7eb005a4c760522118208c4c4c
Wayne Thursby <wayne.thursby@1srq.com>
2019-06-29 20:14:58 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("admin");
+
+  return {
+    templateFormats: ["md", "njk", "html", "liquid"],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
---
+++ pages/home.md
@@ -0,0 +1,11 @@
+---
+layout: layouts/home.njk
+title: Home
+date: 2019-06-29T00:00:00.000Z
+permalink: /
+navtitle: Home
+tags:
+  - nav
+---
+# Welcome to Home
+This is my home page. Welcome.
